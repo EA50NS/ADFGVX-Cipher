e7f6814b88fed2afb3037e07e54ee01f3abe3d8f
Eason Wang <eason@easons-air.home>
2026-06-10 23:21:32 -0400

    Correctly scrambling alphabet and making a unique polybius square

--- ADFGVX.c
+++ ADFGVX.c
@@ -2,35 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
+#include <math.h>
 
 #define ADFGVX "ADFGVX"
-#define ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-char random_char(int index){
-    return ALPHABET[index];
+void random_alpha(){
+    srand(time(NULL));
+    int random_index = 0;
+    char temp;
+    int length = strlen(alphabet);
+
+    for (int i = 0; i < length; i++){
+        random_index = rand() % length; 
+
+        temp = alphabet[random_index];
+        alphabet[random_index] = alphabet[i];
+        alphabet[i] = temp;
+    }
 }
 
 void initialise_polybius_square(){
-    srand(time(NULL));
-    char square[37];
-    int index, i = 0;
-    for(i = 0; i < 36; i++){
-        index = rand() % 36;
-        square[i] = random_char(index);
+    random_alpha();
+    printf("rand alpha test: %s \n", alphabet);
+    char square[6][6];
+
+    int length = sqrt(strlen(alphabet)); // Is this REALLY necessary? Why not just use '6'?
+   
+    int k = 0;
+    for(int i = 0; i < length; i++){
+        for (int j = 0; j < length; j++){
+            square[i][j] = alphabet[k];
+            k++;
+        }
     }
-    square[i] = '\0';
-    
-    printf("testing random string %s\n", square);
+
+    printf("printing square: \n");
+    for (int i = 0; i < length; i++){
+        for (int j = 0; j < length; j++){
+            printf("%c ", square[i][j]);
+        }
+        printf("\n");
+    }
+
+
 }
 
 
 int main (void){
    
-    printf("Initialising Polyibus Square\n");
-    printf(" | A D F G V X\n");
-    printf("--------------\n");
+   // printf("Initialising Polyibus Square\n");
+   // printf(" | A D F G V X\n");
+   // printf("--------------\n");
+   initialise_polybius_square();
     
+
+
 
     return 0;
 }
